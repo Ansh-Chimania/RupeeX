fe24519b52a81b59016db4f8a31c963ef4b781a5
ANSH CHIMANIA <anshchim.work@gmail.com>
2026-04-14 17:59:24 +0530

    Update use-fetch.js

--- hooks/use-fetch.js
+++ hooks/use-fetch.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { toast } from "sonner";
-
+//
 const useFetch = (cb) => {
   const [data, setData] = useState(undefined);
   const [loading, setLoading] = useState(null);
